fb20a72db95734ce291c4e64cb90f2f716953e56
tanjonaaa <hei.tanjona@gmail.com>
2025-08-20 19:39:53 +0300

    Poja Iinit

---
+++ src/main/java/com/company/base/file/hash/FileHash.java
@@ -0,0 +1,6 @@
+package com.company.base.file.hash;
+
+import com.company.base.PojaGenerated;
+
+@PojaGenerated
+public record FileHash(FileHashAlgorithm algorithm, String value) {}
---
+++ src/main/java/com/company/base/file/hash/FileHashAlgorithm.java
@@ -0,0 +1,9 @@
+package com.company.base.file.hash;
+
+import com.company.base.PojaGenerated;
+
+@PojaGenerated
+public enum FileHashAlgorithm {
+  SHA256,
+  NONE
+}
